ecf30466e9a3d22f04fabebd46808626afa6c1c1
zhangleju <1441583226@qq.com>
2019-01-03 15:35:55 +0800

    一些公共方法---zlj

---
+++ src/main/webapp/static/ddms/js/include.js
@@ -0,0 +1,73 @@
+//检查登录
+function checklogin() {
+    $.ajax({
+        type:'POST',
+        url:'/api/checkLogin',
+        dataType:'json',
+        success:function(data){
+            if(data==1) {
+                window.location.href="/api/ranking";
+            }else{
+                alert(data);
+                alertError('请登录');
+            }
+        },
+        error:function(){
+            alertError('系统错误');
+        }
+    });
+}
+//退出登录
+function exit() {
+    $.ajax({
+        url:'api/exituser',
+        type:'POST',
+        success:function(param){
+            if(param == 1){
+                window.location.href = "/api/";
+            }else{
+                alertError(param.msg)
+            }
+        },
+        error:function(){
+            alertError('系统错误')
+        }
+    });
+}
+
+//跳转购买记录页面
+function buy() {
+    $.ajax({
+        url:'selectOrders',
+        type:'POST',
+        success:function(param){
+            if(param == 1){
+                window.location.href = "buylist";
+            }else{
+                alertError(param.msg)
+            }
+        },
+        error:function(){
+            alertError('系统错误')
+        }
+    });
+}
+
+//跳转邀请返利页面
+function share() {
+    $.ajax({
+        url:'subordinate',
+        data:"pn=1",
+        type:'POST',
+        success:function(param){
+            if(param == 1){
+                window.location.href = "share_list";
+            }else{
+                alertError(param.msg)
+            }
+        },
+        error:function(){
+            alertError('系统错误')
+        }
+    });
+}
